1f3412ba02d01ad4b1ebe2ed707c3a7bbaa4e880
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2018-01-09 17:34:38 -0800

    add mission complete widget

--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -27,5 +27,6 @@ protected:
     UFUNCTION()
     void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	
+    USound
 	
 };
