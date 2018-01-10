b7417d563dae87d91a482d27844df7d9399db1d9
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2018-01-09 17:48:40 -0800

    add objective missing sound

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -5,6 +5,7 @@
 #include "Components/DecalComponent.h"
 #include "FPSCharacter.h"
 #include "FPSGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
@@ -28,16 +29,28 @@ AFPSExtractionZone::AFPSExtractionZone()
 
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-    UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone"))
     
     AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
-    if (MyPawn && MyPawn->bIsCarryingObjective == true)
+    if (MyPawn == nullptr)
+    {
+        return;
+    }
+    
+    if (MyPawn->bIsCarryingObjective)
     {
         AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
-        if (GM) {
+        if (GM)
+        {
             GM->CompleteMission(MyPawn);
         }
     }
+    else
+    {
+        UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
+    }
+    UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone"))
+        
+
 }
 
 
--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -27,6 +27,7 @@ protected:
     UFUNCTION()
     void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	
-    USound
+    UPROPERTY(EditDefaultsOnly, Category = "Sounds")
+    USoundBase* ObjectiveMissingSound;
 	
 };
